dbd6896b3929fa80443e9477edfe7ce90e854388
Rafi Adiapratama <rafipratama29@gmail.com>
2021-01-15 22:06:39 +0700

    menambahkan form login

---
+++ login.php
@@ -0,0 +1,24 @@
+<div id="container-user-akses">
+
+    <form action="<?php echo BASE_URL."proses_login.php"; ?>" method="post">
+    <?php 
+        $notif = isset($_GET['notif']) ? $_GET['notif'] : false;
+
+        if($notif == "require") {
+            echo "<div class='notif'>Maaf, email atau password yang kamu masukan tidak cocok.</div>";
+        }
+    ?>
+        <div class="element-form">
+            <label for="email">Email</label>
+            <span><input type="text" name="email" id="email"></span>
+        </div>
+        <div class="element-form">
+            <label for="password">Password</label>
+            <span><input type="password" name="password" id="password"></span>
+        </div>
+        <div class="element-form">
+            <span><input type="submit" value="login"></span>
+        </div>
+    </form>
+
+</div>
