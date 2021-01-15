306c98364577f56800f3c3bc07fbbca352aa3d15
Rafi Adiapratama <rafipratama29@gmail.com>
2021-01-15 20:16:06 +0700

    penambahan sistem pengecekan field register

--- proses_register.php
+++ proses_register.php
@@ -9,8 +9,16 @@ $nama_lengkap = $_POST['nama_lengkap'];
 $email = $_POST['email'];
 $phone = $_POST['phone'];
 $alamat = $_POST['alamat'];
-$password = md5($_POST['password']);
+$password = $_POST['password'];
 $re_password = $_POST['re_password'];
 
-mysqli_query($koneksi, "INSERT INTO user (level, nama, email, alamat, phone, password, status) 
-            VALUES ('$level', '$nama_lengkap', '$email', '$alamat', '$phone', '$password', '$status')");
+unset($_POST['password']);
+unset($_POST['re_password']);
+$data_form = http_build_query($_POST);
+
+if(empty($nama_lengkap) || empty($email) || empty($phone) || empty($alamat) || empty($password)) {
+    header("location: ".BASE_URL."index.php?page=register&notif=require&$data_form");
+} else {
+    mysqli_query($koneksi, "INSERT INTO user (level, nama, email, alamat, phone, password, status) 
+                VALUES ('$level', '$nama_lengkap', '$email', '$alamat', '$phone', '$password', '$status')");
+}
--- register.php
+++ register.php
@@ -1,21 +1,32 @@
 <div id="container-user-akses">
 
     <form action="<?php echo BASE_URL."proses_register.php"; ?>" method="post">
+    <?php 
+        $notif = isset($_GET['notif']) ? $_GET['notif'] : false;
+        $nama_lengkap = isset($_GET['nama_lengkap']) ? $_GET['nama_lengkap'] : false;
+        $email = isset($_GET['email']) ? $_GET['email'] : false;
+        $phone = isset($_GET['phone']) ? $_GET['phone'] : false;
+        $alamat = isset($_GET['alamat']) ? $_GET['alamat'] : false;
+
+        if($notif == "require") {
+            echo "<div class='notif'>Maaf, kamu harus melengkapi form di bawah ini.</div>";
+        }
+    ?>
         <div class="element-form">
             <label for="nama_lengkap">Nama Lengkap</label>
-            <span><input type="text" name="nama_lengkap" id="nama_lengkap"></span>
+            <span><input type="text" name="nama_lengkap" id="nama_lengkap" value="<?php echo $nama_lengkap; ?>"></span>
         </div>
         <div class="element-form">
             <label for="email">Email</label>
-            <span><input type="text" name="email" id="email"></span>
+            <span><input type="text" name="email" id="email" value="<?php echo $email; ?>"></span>
         </div>
         <div class="element-form">
             <label for="phone">No. Handphone</label>
-            <span><input type="text" name="phone" id="phone"></span>
+            <span><input type="text" name="phone" id="phone" value="<?php echo $phone; ?>"></span>
         </div>
         <div class="element-form">
             <label for="alamat">Alamat</label>
-            <span><textarea name="alamat" id="alamat"></textarea></span>
+            <span><textarea name="alamat" id="alamat"><?php echo $alamat; ?></textarea></span>
         </div>
         <div class="element-form">
             <label for="password">Password</label>
