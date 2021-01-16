ae14d7f2248ea8560a50ce736333018c24f1e1fb
Rafi Adiapratama <rafipratama29@gmail.com>
2021-01-16 11:24:19 +0700

    membuat login session

--- proses_login.php
+++ proses_login.php
@@ -12,6 +12,10 @@ if(mysqli_num_rows($query) == 0) {
     header("location: ".BASE_URL."index.php?page=login&notif=true");
 } else {
     $row = mysqli_fetch_assoc($query);
-    echo $row['nama']."<br/>";
-    echo $row['email'];
+
+    session_start();
+    $_SESSION['user_id'] = $row['user_id'];
+    $_SESSION['nama'] = $row['nama'];
+    $_SESSION['level'] = $row['level'];
+    header("location: ".BASE_URL."index.php?page=my_profile&module=pesanan&action=list");
 }
